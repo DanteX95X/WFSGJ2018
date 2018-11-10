0616716d02813dbf177f41652194946f2f73c7f2
DevRaptor <krzysztoftaperek@gmail.com>
2018-11-10 14:31:36 +0100

    Added GameConfig

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -14,13 +14,18 @@ namespace WFS
 	public class GameController : Node
 	{
 		private State state;
-		
+		private ConfigFile config;
 		
 		public override void _Ready()
 		{
 			GD.Print("Controller started");
 			
 			state = new RecordActionsState(new MockPlayer(), 4);
+
+			config = new ConfigFile();
+			config.Load("res://GameConfig.cfg");
+
+			// config.GetValue("Config", "AttackTime");
 		}
 		
 		public override void _Process(float delta)

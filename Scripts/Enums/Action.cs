960939380d8242904e113d66d627142df6efac52
Wowo10 <wojtekwojtek529@gmail.com>
2018-11-10 15:17:10 +0100

    Overloaded Interface.

--- Scripts/Enums/Action.cs
+++ Scripts/Enums/Action.cs
@@ -1,12 +1,13 @@
+using System.ComponentModel;
 
 namespace WFS
 {
-	public enum Action
-	{
-		NegativeSecond = -2,
-		NegativeFirst = -1,
-		Timeout = 0,
-		PositiveFirst = 1,
-		PositiveSecond = 2,
-	}
+    public enum Action
+    {
+        NegativeSecond = -2,
+        NegativeFirst = -1,
+        Timeout = 0,
+        PositiveFirst = 1,
+        PositiveSecond = 2
+    }
 }

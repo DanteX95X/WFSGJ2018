f49d7583b0e7336018e2efade2c237e9fba354d1
Dante <daniellewinski95@gmail.com>
2018-11-12 21:07:25 +0100

    Getting game ready.

--- Scripts/UI/MainMenuButton.cs
+++ Scripts/UI/MainMenuButton.cs
@@ -16,4 +16,23 @@ public class MainMenuButton : Button
 		global.singlePlayer = true;
 		global.GotoScene("res://Scenes/AnimationSingle.tscn");
 	}
+	
+	private void _on_CreditsButton_button_up()
+	{
+		WFS.Global global = (WFS.Global)GetNode("/root/Global");
+		global.singlePlayer = true;
+		global.GotoScene("res://Scenes/Credits.tscn");
+	}
+	
+	private void _on_Button_button_up()
+	{
+		WFS.Global global = (WFS.Global)GetNode("/root/Global");
+		global.singlePlayer = true;
+		global.GotoScene("res://Scenes/MainMenu.tscn");
+	}
+	
+	private void _on_QuitButton_button_up()
+	{
+		GetTree().Quit();
+	}
 }
